d0e1d8e981ff444d136d03dd50e3603f9bd55763
rafaeloliveiraneves <rafaeloliveirans@gmail.com>
2017-12-07 10:32:41 -0200

    feature: start the development of notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  form: FormGroup;
+  
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {  
+    this.createForm();
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      title: ['', [Validators.required]],
+      message: ['', [Validators.required]]
+    })
+  }
+
+  onSubmit(values) {
+    console.log(values)
+  }
 }
